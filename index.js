05b9fa2c5460860e66ff8e5c225f66b0b3425b30
ViniciusMdJ <74380890+ViniciusMdJ@users.noreply.github.com>
2022-12-02 14:17:05 +0000

    add butttons events

--- index.js
+++ index.js
@@ -4,24 +4,57 @@ let TuringContract;
 const Turing_Contract_Address = "";
 const Turing_Contract_ABI = [];
 
-/* 3. Prompt user to sign in to MetaMask */
-const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
-provider.send("eth_requestAccounts", []).then(() => {
-  provider.listAccounts().then((accounts) => {
-    const signer = provider.getSigner(accounts[0]);
+// const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+//   provider.send("eth_requestAccounts", []).then(() => {
+//     provider.listAccounts().then((accounts) => {
+//       const signer = provider.getSigner(accounts[0]);
 
-    /* 3.1 Create instance of Turing smart contract */
-    TuringContract = new ethers.Contract(
-      Turing_Contract_Address,
-      Turing_Contract_ABI,
-      signer
-    );
-  });
-});
+//     /* 3.1 Create instance of Turing smart contract */
+//     TuringContract = new ethers.Contract(
+//       Turing_Contract_Address,
+//       Turing_Contract_ABI,
+//       signer
+//     );
+//   });
+// });
 
-// 4. Creating variables for reusable dom elements
 const SelectPessoa = document.querySelector("#pessoas");
 const qtdTuringsInput = document.querySelector("input[id='qtdTurings']");
 const SelectMultiplier = document.querySelector("#exponential");
 const voteBtn = document.querySelector("#vote");
 const endVotingBtn = document.querySelector("#endVoting");
+
+
+function votar() {
+  var qtdTurings = qtdTuringsInput.value;
+  var pessoa = SelectPessoa.value;
+  var multiplier = SelectMultiplier.value;
+  var qtdTuringsFinal = qtdTurings * multiplier;
+
+  // TuringContract.vote(pessoa, qtdTuringsFinal).then(() => {
+  //   qtdTuringsInput.value = "";
+  // }).catch((err) => {
+  //   alert("Error in voting - " + err.message);
+  // });
+    
+  console.log(pessoa, qtdTuringsFinal);
+}
+
+voteBtn.addEventListener("click", votar);
+
+function changeMultiplier() {
+  var multiplier = SelectMultiplier.value;
+  qtdTuringsInput.setAttribute("max", (2*10**18) / multiplier);
+}
+
+SelectMultiplier.addEventListener("change", changeMultiplier);
+
+function endVoting() {
+  // TuringContract.endVoting().then(() => {
+  //   alert("Voting ended");
+  // }).catch((err) => {
+  //   alert("Error in ending voting - " + err.message);
+  // });
+}
+
+endVotingBtn.addEventListener("click", endVoting);
